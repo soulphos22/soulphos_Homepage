3150abd55da357d65bf4b109ef33c7a4701950ab
vpvm96 <vpvm96@gmail.com>
2023-06-06 20:22:54 +0900

    feat: Footer 기본 레이아웃 완성

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        borderTop: '1px solid #eee',
+        padding: '1rem',
+      }}
+    >
+      <Typography variant="p">(주) 솔포스</Typography>
+      <Typography variant="p">MAIL: TestEmail@gmail.com</Typography>
+      <Typography variant="p">
+        COPYRIGHT ⓒ SOULPHOS COMPANY. ALL RIGHTS RESERVED.
+      </Typography>
+    </Box>
+  );
 }
