9c10198670059807a5251ce85446322abe39994c
vpvm96 <vpvm96@gmail.com>
2023-06-06 23:35:48 +0900

    feat: ItemList, QnaLink, Report 컴포넌트 최상위 페이지 적용

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,24 @@
 import { Box } from '@mui/material';
+import { itemObj } from '../utils/itemObj';
 
 import Banner from '../components/homepage/Banner';
+import ItemList from '../components/homepage/ItemList';
+import QnaLink from '../components/homepage/QnaLink';
+import Report from '../components/homepage/Report';
 
 export default function HomePage() {
   return (
-    <Box sx={{ height: '100vh' }}>
+    <Box>
       <Banner />
+      <Box
+        sx={{ paddingTop: '5rem', width: '100%', backgroundColor: '#fafaff' }}
+      >
+        {itemObj.map((item) => (
+          <ItemList key={item.id} textObj={item} />
+        ))}
+      </Box>
+      <QnaLink />
+      <Report />
     </Box>
   );
 }
