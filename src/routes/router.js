02e0924c18a35d1c5c624919efaa7b9c63d1deaf
vpvm96 <vpvm96@gmail.com>
2023-06-07 20:16:23 +0900

    feat: Routing용 컴포넌트 추가 및 적용

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,11 +8,19 @@ import {
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
+import ProductPage from '../pages/ProductPage';
+import DownloadPage from '../pages/DownloadPage';
+import FaqPage from '../pages/FaqPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<HomePage />}></Route>
+      <Route path="about" element={<AboutPage />}></Route>
+      <Route path="product" element={<ProductPage />}></Route>
+      <Route path="download" element={<DownloadPage />}></Route>
+      <Route path="faq" element={<FaqPage />}></Route>
     </Route>,
   ),
 );
