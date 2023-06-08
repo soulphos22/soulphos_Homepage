0ee58de63ebcd97d98e8d0f18085e1e727c1a57e
vpvm96 <vpvm96@gmail.com>
2023-06-08 21:57:28 +0900

    fix: Report 컴포넌트 css 수정

--- src/components/homepage/Report.jsx
+++ src/components/homepage/Report.jsx
@@ -20,9 +20,25 @@ export default function Report() {
           신종 및 변종 또는 제거되지 않는 악성코드가 존재하거나 사용 중 버그
           또는 오진이 발생하면 아래 전자우편주소를 통해 바로 알려주세요.
         </Typography>
-        <Typography variant="h5" sx={{ fontWeight: 'lighter' }}>
-          이메일: <a href="mailto:test123@gmail.com">test123@gmail.com</a>
-        </Typography>
+        <Box
+          sx={{
+            width: '65%',
+            border: '1px solid #000000',
+            p: '25px 0px',
+            borderRadius: '8px',
+            textAlign: 'center',
+          }}
+        >
+          <Typography variant="h5" sx={{ fontWeight: 'lighter' }}>
+            이메일:&nbsp;
+            <a
+              href="mailto:test123@gmail.com"
+              style={{ textDecoration: 'none' }}
+            >
+              TestEmail123@gmail.com
+            </a>
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
